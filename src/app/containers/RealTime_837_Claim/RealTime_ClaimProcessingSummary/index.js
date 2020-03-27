2f5d58871e39a7f3ae82680faa03d6851e95785d
Vaibhav Ghatolkar <vaibhav.ghatolkar@devcool.com>
2020-03-27 16:19:02 +0530

    Claim processing changes

--- src/app/containers/RealTime_837_Claim/RealTime_ClaimProcessingSummary/index.js
+++ src/app/containers/RealTime_837_Claim/RealTime_ClaimProcessingSummary/index.js
@@ -196,6 +196,7 @@ export class ClaimProcessingSummary extends React.Component {
             Claim837RTDashboardCountClaimStatus(Sender:"${this.state.selectedTradingPartner}",State:"${this.state.State}",Provider:"${this.state.providerName}",StartDt:"",EndDt:"",Type:"${this.state.type}", RecType: "Inbound") {
                 X12Count
                 HiPaaSCount
+                MCGLoadCount
             }
             Claim837RTDashboardTable(Sender:"${this.state.selectedTradingPartner}",State:"${this.state.State}",Provider:"${this.state.providerName}",StartDt:"",EndDt:"",Type:"${this.state.type}", RecType: "Inbound") {
                 Accepted_Claims
@@ -236,7 +237,7 @@ export class ClaimProcessingSummary extends React.Component {
                         FileReject_Claims: _data ? _data.FileReject_Claims : 0,
                         Processing_Claims: _data ? _data.Processing_Claims : 0,
                         ReconciledError_Claims: _data ? _data.ReconciledError_Claims : 0,
-                        LoadingClaims: _data ? _data.LoadingClaims : 0,
+                        LoadingClaims: _data ? data.MCGLoadCount : 0,
                         LoadedErrorClaims: _data ? _data.LoadedErrorClaims : 0
                     })
                 }
