a9e1bfe3004b0874ff0dc77fa2eb5403143b14ea
swapnilkhadke <swapnilkhadke@devcool.com>
2020-04-01 14:13:39 +0530

    Added changes

--- src/app/containers/RealTime_837_Claim/RealTime_ClaimProcessingSummary/index.js
+++ src/app/containers/RealTime_837_Claim/RealTime_ClaimProcessingSummary/index.js
@@ -201,13 +201,16 @@ export class ClaimProcessingSummary extends React.Component {
     }
 
     getClaimCounts = async () => {
+        let startDate = this.state.startDate ? moment(this.state.startDate).format('YYYY-MM-DD') : ""
+        let endDate = this.state.endDate ? moment(this.state.endDate).format('YYYY-MM-DD') : ""
+
         let query = `{
-            Claim837RTDashboardCountClaimStatus(Sender:"${this.state.selectedTradingPartner}",State:"${this.state.State}",Provider:"${this.state.providerName}",StartDt:"",EndDt:"",Type:"${this.state.type}", RecType: "Inbound") {
+            Claim837RTDashboardCountClaimStatus(Sender:"${this.state.selectedTradingPartner}",State:"${this.state.State}",Provider:"${this.state.providerName}",StartDt:"${startDate}",EndDt:"${endDate}",Type:"${this.state.type}", RecType: "Inbound") {
                 X12Count
                 HiPaaSCount
                 MCGLoadCount
             }
-            Claim837RTDashboardTable(Sender:"${this.state.selectedTradingPartner}",State:"${this.state.State}",Provider:"${this.state.providerName}",StartDt:"",EndDt:"",Type:"${this.state.type}", RecType: "Inbound") {
+            Claim837RTDashboardTable(Sender:"${this.state.selectedTradingPartner}",State:"${this.state.State}",Provider:"${this.state.providerName}",StartDt:"${startDate}",EndDt:"${endDate}",Type:"${this.state.type}", RecType: "Inbound") {
                 Accepted_Claims
                 Rejected_Claims
                 FileReject_Claims
