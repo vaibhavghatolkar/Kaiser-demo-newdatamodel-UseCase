615885d85a08149c0f9767924dc8cee437593bc1
swapnilkhadke <swapnilkhadke@hipaas.com>
2020-06-22 10:52:46 +0530

    Added changes

--- src/app/containers/Enrollment/Enrollment_Outbound/index.js
+++ src/app/containers/Enrollment/Enrollment_Outbound/index.js
@@ -335,9 +335,7 @@ export class Enrollment_Outbound extends React.Component {
                 console.log("al;fjsdjfjsh", res.data.Dashboard834FileDetailsOutbound)
                 this.setState({
                     rowData: res.data.Dashboard834FileDetailsOutbound,
-
                 })
-
             })
             .catch(err => {
                 process.env.NODE_ENV == 'development' && console.log(err)
@@ -873,7 +871,7 @@ export class Enrollment_Outbound extends React.Component {
                 TotalError
                 Resubmit
                 }
-                FileLevelCount834 {
+                FileLevelCount834 (State:"${this.state.State}", StartDt :"${startDate}", EndDt : "${endDate}"){
                     TotalCount
                     Validated
                     Error
@@ -1639,7 +1637,7 @@ export class Enrollment_Outbound extends React.Component {
 
         let columnDefs = [
             { headerName: "File Name", field: "FileName", width: 250, cellStyle: { wordBreak: 'break-all', 'white-space': 'normal', color: '#139DC9', cursor: 'pointer' } },
-            { headerName: "Process Id", field: "FileID",  width: 130},
+            { headerName: "Process Id", field: "FileID", width: 130 },
             { headerName: "Sent Date", field: "Date", width: 200, cellStyle: { wordBreak: 'break-all', 'white-space': 'normal' } },
             // { headerName: "Submitter", field: "Subscriber",width:120, cellStyle: { wordBreak: 'break-all', 'white-space': 'normal' }  },
             { headerName: "Total Enrollments", field: "Enrollment", width: 150, cellStyle: { wordBreak: 'break-all', 'white-space': 'normal' } },
